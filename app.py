93e0ce6fd2f27fcbf96c6301bf49bbeafe12bed7
jesusrubio3 <jesusrubiomartin3@gmail.com>
2021-04-19 19:37:49 +0200

    creación página potencia

--- app.py
+++ app.py
@@ -6,6 +6,17 @@ app = Flask(__name__)
 def inicio():
     return render_template("inicio.html")
 
+@app.route('/potencia/<base>/<exponente>')
+def potencia(base,exponente):
+    try:
+        base=int(base)
+        exponente=int(exponente)
+        resultado=base**exponente
+    except:
+        abort(404)
+    return render_template("potencia.html",base=base,exponente=exponente,result$
+
+
 
 app.run("0.0.0.0",8000,debug=True)
 
